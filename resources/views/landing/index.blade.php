5ce78b06a27d5aae3a39ab7317883a48a97af934
aryawardhana15 <wardhanahafiz567@gmail.com>
2025-03-21 22:16:31 +0700

    feat: add authentication system

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -48,6 +48,7 @@
 @push('scripts-dependencies')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="/js/landing.js"></script>
+
 @endpush
 
 @section("content")
